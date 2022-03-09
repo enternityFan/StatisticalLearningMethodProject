d6182859f6e51fbd0151ac37826ad677d3609fa4
enternityFan <51934710+enternityFan@users.noreply.github.com>
2022-03-09 15:11:58 +0800

    前很多章的代码

---
+++ 第六章：逻辑斯蒂回归/test.py
@@ -0,0 +1,22 @@
+# @Time : 2022-03-09 8:35
+# @Author : Phalange
+# @File : test.py
+# @Software: PyCharm
+# C'est la vie,enjoy it! :D
+
+
+def printTriangle(n):
+    for i in range(n):
+        print(" " * (n - i) + "*" * (2 * i - 1))
+
+
+
+
+
+if __name__ == "__main__":
+    printTriangle(4)
+
+
+
+
+
